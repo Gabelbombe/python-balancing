d39a024fbeb04f571bb13b60d5dd633cbeb117ab
Till Eulenspiegel <niigata@Niigata-ken.local>
2017-04-27 13:24:17 -0700

    Adding definition

--- balancer.py
+++ balancer.py
@@ -4,7 +4,6 @@ i = iter('(){}[]<>')  # iterative parenthesis
 p = dict(zip(i, i))   # create dictionary from i's tuples
 c = p.values()
 
-print("\nI:") ; print(list(i))
-print("\nP:") ; print(list(p))
-print("\nC:") ; print(list(c))
 
+def balancer(input):
+    return False
