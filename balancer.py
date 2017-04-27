a1da237cd1157837833b226ed427ca8260211645
Till Eulenspiegel <niigata@Niigata-ken.local>
2017-04-27 13:22:14 -0700

    Listing outputs

--- balancer.py
+++ balancer.py
@@ -2,3 +2,9 @@
 
 i = iter('(){}[]<>')  # iterative parenthesis
 p = dict(zip(i, i))   # create dictionary from i's tuples
+c = p.values()
+
+print("\nI:") ; print(list(i))
+print("\nP:") ; print(list(p))
+print("\nC:") ; print(list(c))
+
