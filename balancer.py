f2efd80f263156391e682162112269281ec42d44
Till Eulenspiegel <niigata@Niigata-ken.local>
2017-04-27 13:29:28 -0700

    iter start and lookup stash

--- balancer.py
+++ balancer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
-i = iter('(){}[]<>')  # iterative parenthesis
-p = dict(zip(i, i))   # create dictionary from i's tuples
-c = p.values()
+i = iter('(){}[]<>')        # iterative parenthesis
+p = dict(zip(i, i))         # create dictionary from i's tuples
+c = p.values()              # create values (aka, opp neighbor)
 
 
 def balancer(input):
-    s = []            # stack
+    s = []                  # as stack
+    for c in input:         # iterate through our input
+        d = p.get(c, None)  # get parenthesis' val (opp neighbor)
+        if d:
+            s.append(d)     # append paren to lookup
     return s
